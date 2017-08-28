90544af638067981945742db213a3216f414a729
AndreasSterner <andreas@sterner.io>
2017-08-29 00:01:41 +0200

    Initial Commit

---
+++ Javascript/master.js
@@ -0,0 +1,9 @@
+var firstName = "Andreas";
+var lastName = "Sterner";
+var age = "17";
+
+console.log("First name " + firstName);
+console.log("Last name " + lastName);
+console.log(age);
+
+alert("Printed name & age in console")
