be2ba52945269046dc1c5903114bb171f8bce3be
AndreasSterner <andreas@sterner.io>
2017-09-07 23:10:20 +0200

    uhhh...

--- Javascript/basicDiceGame.js
+++ Javascript/basicDiceGame.js
@@ -1,1 +1,20 @@
 console.log("test"); // works
+
+
+document.getElementById('playerNameSelect1').onclick = function() {
+
+    var nameInput = "";
+    
+    nameInput = document.getElementById(`playerNameInput1`).value;
+
+    document.getElementById(`playerName1`).innerHTML = nameInput;
+}
+
+document.getElementById('playerNameSelect2').onclick = function() {
+
+    var nameInput = "";
+
+    nameInput = document.getElementById(`playerNameInput2`).value;
+
+    document.getElementById(`playerName2`).innerHTML = nameInput;
+}
