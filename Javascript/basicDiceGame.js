0abc1b190fa123b3115fbb04d658e0530e4a96f0
AndreasSterner <andreas@sterner.io>
2017-09-08 17:42:05 +0200

    name change done

--- Javascript/basicDiceGame.js
+++ Javascript/basicDiceGame.js
@@ -1,1 +1,11 @@
 console.log("test"); // works
+
+document.getElementById('playerNameSelect1').onclick = function() {
+
+  document.getElementById('playerName1').innerHTML = document.getElementById('playerNameInput1').value;
+}
+
+document.getElementById('playerNameSelect2').onclick = function() {
+
+  document.getElementById('playerName2').innerHTML = document.getElementById('playerNameInput2').value;
+}
