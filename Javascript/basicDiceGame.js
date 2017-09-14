64a77815c2cf423341ae463362df102835cec519
AndreasSterner <andreas@sterner.io>
2017-09-14 12:31:43 +0200

    immprovments

--- Javascript/basicDiceGame.js
+++ Javascript/basicDiceGame.js
@@ -26,43 +26,50 @@ document.getElementById('playerNameSelect2').onclick = function() {
 
 //Player 1
 document.getElementById('playerRoll1').onclick = function() {
+  $('#addPointsPlayer1').css('display', 'block');
   rollNumber = Math.floor((Math.random() * 6) + 1);
   console.log('before', rollNumber);
   if (rollNumber == 1) {
     document.getElementById('addPointsPlayer1').innerHTML = `-${playerTotalNumber1}`;
+    $('#addPointsPlayer1').attr('src', `../CSS/Assets/Dices/dice-${rollNumber}.png`);
     playerTotalNumber1 = 0;
     document.getElementById('playerPoints1').innerHTML = playerTotalNumber1;
+
 
   } else if (rollNumber != 1){
 
     playerTotalNumber1 = rollNumber + playerTotalNumber1;
-    console.log('after', rollNumber);
-    document.getElementById('addPointsPlayer1').innerHTML = `+${rollNumber}`;
+    $('#addPointsPlayer1').attr('src', `../CSS/Assets/Dices/dice-${rollNumber}.png`);
     document.getElementById('playerPoints1').innerHTML = playerTotalNumber1;
 
     if (playerTotalNumber1 > 50) {
       document.getElementById('playerName1').innerHTML = `${playerName1Var} - Winner!`;
+
     }
 
   }
 }
 //Player 2
 document.getElementById('playerRoll2').onclick = function() {
+  $('#addPointsPlayer2').css('display', 'block');
   rollNumber = Math.floor((Math.random() * 6) + 1);
   console.log('before', rollNumber);
   if (rollNumber == 1) {
     document.getElementById('addPointsPlayer2').innerHTML = `-${playerTotalNumber2}`;
+    $('#addPointsPlayer2').attr('src', `../CSS/Assets/Dices/dice-${rollNumber}.png`);
     playerTotalNumber2 = 0;
     document.getElementById('playerPoints2').innerHTML = playerTotalNumber2;
   } else if (rollNumber != 1){
 
     playerTotalNumber2 = rollNumber + playerTotalNumber2;
     console.log('after', rollNumber);
-    document.getElementById('addPointsPlayer2').innerHTML = `+${rollNumber}`;
+    $('#addPointsPlayer2').attr('src', `../CSS/Assets/Dices/dice-${rollNumber}.png`);
     document.getElementById('playerPoints2').innerHTML = playerTotalNumber2;
 
     if (playerTotalNumber1 > 50) {
       document.getElementById('playerName1').innerHTML = `${playerName2Var} - Winner!`;
+
+
     }
 
   }
